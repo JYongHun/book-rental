c9a15d0dec34c07ab6141dadc04b39ae8617cace
JYongHun <jauo40@gmail.com>
2020-08-05 12:31:50 +0900

    123

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
+const morgan = require("morgan");
+
+const users = require("./routes/users");
+const rentalj = require("./routes/rental");
 
 const app = express();
 
 app.use(express.json());
 
+app.use(morgan("common"));
+
+app.use("/api/v1/booksuserj", users);
+app.use("/api/v1/booksrentalj", rentalj);
 const PORT = process.env.PORTJ || 5551;
 app.listen(PORT, console.log("JYP SERVER"));
