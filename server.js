a54a6c1ee81df3e4bab71f83d93764e50f99bac1
JYongHun <jauo40@gmail.com>
2020-08-05 11:47:41 +0900

    책대여반납 추가

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const morgan = require("morgan");
+
 const users = require("./routes/users");
+const rentalj = require("./routes/rental");
+
 const app = express();
 
 app.use(express.json());
 
 app.use(morgan("common"));
 
-//The A took a few hostages and tried to negotiate
 app.use("/api/v1/booksuserj", users);
+app.use("/api/v1/booksrentalj", rentalj);
 const PORT = process.env.PORTJ || 5551;
 app.listen(PORT, console.log("JYP SERVER"));
